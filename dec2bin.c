cc5cdaf720b834c1b5f8a1213f2201808426c22e
pepe-16 <36467892+pepe-16@users.noreply.github.com>
2018-02-14 23:20:26 -0600

    Update dec2bin.c
    Version mas comentada de la función proceso.

--- dec2bin.c
+++ dec2bin.c
@@ -1,12 +1,12 @@
 //Programa hecho por Liber Adrián Hernández Abad
-
+//Uso de bibliotecas estandares
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void proceso (long dec);
+void proceso (long dec);//Prototipo de funcion destina para la conversion decimal-binario.
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])//Funcion principal 
 {
 	char *apuntador; //Variable que recibe la parte de cadena del arreglo
 	long dec; //Variable que recibe la parte numerica del arreglo
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	proceso(dec);
 }
 
-void proceso (long dec)
+void proceso (long dec)//Declaracion de la funcion destina para la conversion decimal-binario.
 {
 	printf("%ld en binario es: ",dec);
 	while (dec!=0)
